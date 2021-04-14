20ca962c234f3f8c40c446d8be1420f7200c4081
Igor Vukic <igorvukic@gmail.com>
2021-04-15 00:48:11 +0200

    Add loader on chart view

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,7 +1,13 @@
 <template>
   <v-container>
+    <v-skeleton-loader
+      type="image"
+      v-if="loading"
+      height="500"
+    ></v-skeleton-loader>
     <vue-frappe
       id="test"
+      v-else
       :labels="chartData.labels"
       type="axis-mixed"
       :height="500"
@@ -12,7 +18,6 @@
   </v-container>
 </template>
 <script>
-//TODO Loader
 import { VueFrappe } from "vue2-frappe";
 import { mapState } from "vuex";
 
@@ -52,7 +57,17 @@ export default {
       });
       return { labels, mappedData };
     },
+    loading() {
+      return !!this.$store.state.loading;
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+::v-deep .v-skeleton-loader__image.v-skeleton-loader__bone {
+  height: 500px;
+}
+</style>
+
 
