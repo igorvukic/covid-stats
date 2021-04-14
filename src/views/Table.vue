2b0060d939dd44a0e36cdf47f1696961dfa7e712
Igor Vukic <igorvukic@gmail.com>
2021-04-14 13:49:04 +0200

    Fixing multiple loading initial state issue

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -9,21 +9,27 @@
       :loading="loading"
       loading-text="Loading countries data..."
     >
-    <template v-slot:item.TotalConfirmed="{ item }">
-      {{item.TotalConfirmed | formatNumber}}
-    </template>
-    <template v-slot:item.TotalDeaths="{ item }">
-      {{item.TotalDeaths | formatNumber}}
-    </template>
-    <template v-slot:item.TotalRecovered="{ item }">
-      {{item.TotalRecovered | formatNumber}}
-    </template>
+      <template v-slot:item.TotalConfirmed="{ item }">
+        {{ item.TotalConfirmed | formatNumber }}
+      </template>
+      <template v-slot:item.TotalDeaths="{ item }">
+        {{ item.TotalDeaths | formatNumber }}
+      </template>
+      <template v-slot:item.TotalRecovered="{ item }">
+        {{ item.TotalRecovered | formatNumber }}
+      </template>
       <template slot="body.append">
         <tr>
           <th class="body-2 text-uppercase font-weight-bold">Totals</th>
-          <th class="body-2  font-weight-bold text-right">{{ total.confirmed | formatNumber }}</th>
-          <th class="body-2  font-weight-bold text-right">{{ total.deaths | formatNumber}}</th>
-          <th class="body-2  font-weight-bold text-right">{{ total.recovered | formatNumber}}</th>
+          <th class="body-2 font-weight-bold text-right">
+            {{ total.confirmed | formatNumber }}
+          </th>
+          <th class="body-2 font-weight-bold text-right">
+            {{ total.deaths | formatNumber }}
+          </th>
+          <th class="body-2 font-weight-bold text-right">
+            {{ total.recovered | formatNumber }}
+          </th>
         </tr>
       </template>
     </v-data-table>
@@ -36,33 +42,39 @@ export default {
   data() {
     return {
       headers: [
-        { text: "Country", value: 'Country'},
-        { text: "Total Confirmed", value: 'TotalConfirmed', align: 'end' },
-        { text: "Total Deaths", value: 'TotalDeaths', align: 'end' },
-        { text: "Total Recovered", value: 'TotalRecovered', align: 'end' },
+        { text: "Country", value: "Country" },
+        { text: "Total Confirmed", value: "TotalConfirmed", align: "end" },
+        { text: "Total Deaths", value: "TotalDeaths", align: "end" },
+        { text: "Total Recovered", value: "TotalRecovered", align: "end" },
       ],
       loading: false,
     };
   },
   computed: {
     ...mapState(["filteredCountriesData"]),
-    total(){
+    total() {
       const reducer = (acc, cur) => {
         acc.confirmed += cur.TotalConfirmed;
         acc.deaths += cur.TotalDeaths;
         acc.recovered += cur.TotalRecovered;
         return acc;
-      }
-      return this.filteredCountriesData.reduce(reducer,{confirmed: 0, deaths: 0, recovered: 0})
-    }
+      };
+      return this.filteredCountriesData.reduce(reducer, {
+        confirmed: 0,
+        deaths: 0,
+        recovered: 0,
+      });
+    },
   },
   methods: {
     ...mapActions(["getCountriesData"]),
 
     async loadCountriesData() {
-      this.loading = true;
-      await this.getCountriesData();
-      this.loading = false;
+      if (!this.filteredCountriesData.length) {
+        this.loading = true;
+        await this.getCountriesData();
+        this.loading = false;
+      }
     },
   },
   async created() {
